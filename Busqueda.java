eb65d44e0e0365ddcb37b2a266bc6f99c7d03e6a
MateoRamirezRubio1 <100296963+MateoRamirezRubio1@users.noreply.github.com>
2022-06-06 04:22:03 -0500

    Update Busqueda.java

--- Busqueda.java
+++ Busqueda.java
@@ -3,15 +3,15 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Busqueda {
-    public ArrayList<String> leerDatosArchivo(String nombreArchivoCSV){
-        ArrayList<String> palabras = new ArrayList<String>();
+    public <T> ArrayList<T> leerDatosArchivo(T nombreArchivoCSV){
+        ArrayList<T> palabras = new ArrayList<>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(nombreArchivoCSV));
-            String line = null;
-            String[] parts={};
+            BufferedReader reader = new BufferedReader(new FileReader(nombreArchivoCSV.toString()));
+            var line = "";
+            T[] parts;
 
             while ((line = reader.readLine()) != null) {
-                parts = line.split(";");
+                parts = (T[]) line.split(";");
 
                 palabras.add(parts[0]);
             }
@@ -23,27 +23,40 @@ public class Busqueda {
         return palabras;
     }
 
-    public ArrayList<Object> masRelevantes(ArrayList<String> arr, Character num){
-        Hashtable<String, Integer> contenedor = new Hashtable<String, Integer>();
-        ArrayList<String> relevantes = new ArrayList<String>();
-        ArrayList<Object> topMasRelevantes = new ArrayList<Object>();
-        int contador = 0;
-        String temporal = "";
+    public <T> ArrayList<T> masRelevantes(ArrayList<T> arr, T usuario, T opcion){
+        var contenedor = new Hashtable<>();
+        ArrayList<String> relevantes = new ArrayList<>();
+        ArrayList<T> topMasRelevantes = new ArrayList<>();
+        var contador = 0;
+        var temporal = "";
+        T[] parts;
 
-        for(int i=0; i< arr.size();i++){
-            for(int j=0;j<arr.get(i).length();j++){
-                if(Character.compare(arr.get(i).charAt(j), num) == 0 ){
-                    relevantes.add(arr.get(i));
+        //Obtener busquedas con el dígito o palabra necesitado
+        if(opcion.equals("a")){
+            for(var i=0; i< arr.size();i++){
+                for(var j=0;j<arr.get(i).toString().length();j++){
+                    if(Character.compare(arr.get(i).toString().charAt(j), usuario.toString().charAt(0)) == 0 ){
+                        relevantes.add(arr.get(i).toString());
+                    }
+                }
+            }
+        }else {
+            for(var i=0; i< arr.size();i++){
+                parts = (T[]) arr.get(i).toString().split(" ");
+                for(var j=0;j< parts.length;j++){
+                    if(parts[j].toString().equals(usuario)){
+                        relevantes.add(arr.get(i).toString());
+                    }
                 }
             }
         }
-
 
         if(relevantes.size() != 0){
             Collections.sort(relevantes);
             temporal = relevantes.get(0);
 
-            for(int i=0;i<relevantes.size();i++){
+            //Obtener las veces que se repite cada busqueda
+            for(var i=0;i<relevantes.size();i++){
                 contador+=1;
                 if(temporal.equals(relevantes.get(i))){
                     contenedor.put(relevantes.get(i), contador);
@@ -57,19 +70,20 @@ public class Busqueda {
             contador = 0;
             relevantes.clear();
 
-            for(int i=0;i<3;i++){
+            //Obtener los que se repiten más veces en orden descendente
+            for(var i=0;i<5;i++){
                 Enumeration e = contenedor.keys();
-                Object clave="";
-                Object palabra="";
+                var clave="";
+                var palabra="";
                 while( e.hasMoreElements() ) {
-                    clave = e.nextElement();
-                    if (contenedor.get(clave)>contador){
+                    clave = e.nextElement().toString();
+                    if ((Integer) contenedor.get(clave)>contador){
                         palabra = clave;
-                        contador = contenedor.get(clave);
+                        contador = (Integer) contenedor.get(clave);
 
                     }
                 }
-                topMasRelevantes.add(palabra);
+                topMasRelevantes.add((T) palabra);
                 contador=0;
                 contenedor.remove(palabra);
             }
@@ -78,13 +92,20 @@ public class Busqueda {
         return topMasRelevantes;
     }
 
-    public void imprimirTopRelevantes(ArrayList<Object> arr){
+    public <T> void imprimirTopRelevantes(ArrayList<T> arr, T opcion){
         if (arr.size()==0){
             System.out.println("No sucedió ningún hecho relevante con la opción ingresada");
         }else{
-            for (int i=0;i<3;i++){
-                System.out.print(i+1);
-                System.out.println(": " + arr.get(i));
+            if(opcion.equals("a")){
+                for (var i=0;i<3;i++){
+                    System.out.print(i+1);
+                    System.out.println(": " + arr.get(i));
+                }
+            } else {
+                for (var i=0;i<5;i++){
+                    System.out.print(i+1);
+                    System.out.println(": " + arr.get(i));
+                }
             }
         }
     }
@@ -92,7 +113,7 @@ public class Busqueda {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         Busqueda datosArchivo = new Busqueda();
-        ArrayList<String> datos = new ArrayList<String>();
+        var datos = new ArrayList<>();
         datos = datosArchivo.leerDatosArchivo("C:\\Users\\PC Mateo\\IdeaProjects\\Buscador\\datos.csv");
 
         System.out.println("Bienvenido:");
@@ -102,15 +123,18 @@ public class Busqueda {
         System.out.println("b.) Mostrar 5 hechos relevantes según una palabra dado por el Usuario.");
         System.out.println("c.) Mostrar 5 hechos relevantes según un texto dado por el Usuario.");
 
-        String opcionUsuario = input.nextLine();
+        var opcionUsuario = input.nextLine();
 
         switch (opcionUsuario){
             case "a":
                 System.out.print("Ingrese el número a buscar: ");
-                String numUsuario1 = input.nextLine();
-                datosArchivo.imprimirTopRelevantes(datosArchivo.masRelevantes(datos, numUsuario1.charAt(0)));
+                var numUsuario = input.nextLine();
+                datosArchivo.imprimirTopRelevantes(datosArchivo.masRelevantes(datos, numUsuario.charAt(0), "a"), "a");
                 break;
             case "b":
+                System.out.print("Ingrese la palabra a buscar: ");
+                var palabraUsuario = input.nextLine();
+                datosArchivo.imprimirTopRelevantes(datosArchivo.masRelevantes(datos, palabraUsuario, "b"), "b");
                 break;
             case "c":
                 break;
